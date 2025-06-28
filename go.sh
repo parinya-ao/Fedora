e8133e36eac8ada1cfcd50dd3407f590a7341fbc
Parinya_Aobaun <159911463+parinya-ao@users.noreply.github.com>
2025-06-28 10:41:46 +0700

    Update go.sh

--- go.sh
+++ go.sh
@@ -4,3 +4,6 @@ go install golang.org/x/tools/cmd/goimports@latest
 go install golang.org/x/lint/golint@latest
 go install golang.org/x/tools/cmd/godoc@latest
 go install github.com/go-delve/delve/cmd/dlv@latest
+
+echo 'export PATH=$PATH:~/go/bin' >> ~/.bashrc
+echo 'export PATH=$PATH:~/go/bin' >> ~/.config/fish/config.fish
