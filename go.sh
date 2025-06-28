aa6fcf3c1625da82eb0cb5cd2d12eb7e2b3cc2a5
Parinya_Aobaun <159911463+parinya-ao@users.noreply.github.com>
2025-06-28 10:41:07 +0700

    Create go.sh

---
+++ go.sh
@@ -0,0 +1,6 @@
+sudo dnf install golang -y
+go install golang.org/x/tools/gopls@latest
+go install golang.org/x/tools/cmd/goimports@latest
+go install golang.org/x/lint/golint@latest
+go install golang.org/x/tools/cmd/godoc@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
