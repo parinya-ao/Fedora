e59400981c65dcef1d881f36e56d244e80470ef1
Parinya_Aobaun <159911463+parinya-ao@users.noreply.github.com>
2025-08-09 09:25:42 +0700

    Create fish.fish

---
+++ fish.fish
@@ -0,0 +1,33 @@
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+// plugin
+fisher install IlanCosman/tide
+fisher install franciscolourenco/done
+fisher install jorgebucaran/autopair.fish
+fisher install jorgebucaran/nvm.fish
+fisher install reitzig/sdkman-for-fish
+fisher install kidonng/zoxide.fish
+fisher install PatrickF1/fzf.fish
+fisher install edc/bass
+fisher install gazorby/fish-abbreviation-tips
+
+fisher install laughedelic/pisces           
+fisher install jethrokuan/z                 
+fisher install oh-my-fish/plugin-bang-bang  
+fisher install PatrickF1/colored_man_pages  
+fisher install jorgebucaran/hydro           
+fisher install meaningful-ooo/sponge        
+fisher install nickeb96/puffer-fish         
+fisher install decors/fish-colored-man      
+fisher install jhillyerd/plugin-git         
+fisher install oh-my-fish/plugin-docker-compose 
+fisher install evanlucas/fish-kubectl-completions 
+fisher install derailed/k9s                 
+fisher install bobthecow/git-flow-completion 
+fisher install halostatue/fish-direnv       
+fisher install joseluisq/gitnow             
+fisher install gazorby/fish-exa             
+fisher install rayx/fish-docker             
+fisher install lgathy/google-cloud-sdk-fish-completion 
+
+\
